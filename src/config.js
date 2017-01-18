75337ea96f7ef705c08e8ad5880611b3f466e26a
Jean Farrugia <jeanfrg@gmail.com>
2017-01-18 10:49:20 +0100

    config docs

--- src/config.js
+++ src/config.js
@@ -1,20 +1,26 @@
-
 module.exports = {
-	BG_COLOR: 0x222222,
-	BARS_WIDTH: 15,
-	LINES_DISTANCE: 20,
-	PLAYER_MARGIN: 10,
-	PLAYER_COLOR: 0xEEEEEE,
-	SCORES_MARGIN: { x: 30, y: 30 },
-	TEXT_STYLE: {
+    BG_COLOR: 0x222222,
+    BARS_WIDTH: 20,
     BARS_HEIGHT: 150,
+    BARS_BORDER_RADIUS: 100,
+
+    PLAYER_MARGIN: 20,
+    PLAYER_COLOR: 0xEEEEEE,
+
+    SCORES_MARGIN: { x: 30, y: 30 },
+
+    TEXT_STYLE: {
         font: '60px Helvetica, Arial, sans-serif',
         fill: '#eee',
         align: 'center'
     },
+
+    // paddle line distance from edges
+    LINES_DISTANCE: 20,
     LINES_COLOR: 0xEEEEEE,
     LINES_WIDTH: 10,
+
     BALL_COLOR: 0xEEEEEE,
     BALL_SIZE: 10,
-    BALL_SPEED: 15
+    BALL_SPEED: 20
 };
