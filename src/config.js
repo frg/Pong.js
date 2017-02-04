a797c83e1b8e26400b0f9e9d64d791341b0e4500
Jean Farrugia <jeanfrg@gmail.com>
2017-02-04 23:09:17 +0100

    config update

--- src/config.js
+++ src/config.js
@@ -2,13 +2,15 @@ module.exports = {
     BG_COLOR: 0x222222,
     BARS_WIDTH: 20,
     BARS_HEIGHT: 150,
-    BARS_BORDER_RADIUS: 100,
+    BARS_BORDER_RADIUS: 10,
 
     PLAYER_SPEED: 500,
     PLAYER_MARGIN: 20,
     PLAYER_COLOR: 0xEEEEEE,
 
-    SCORES_MARGIN: { x: 40, y: 10 },
+    SCORES_MARGIN: { x: 80, y: 0 },
+    SCORES_BACKGROUND_COLOR: 0xEEEEEE,
+    SCORES_BACKGROUND_HEIGHT: 100,
 
     TEXT_STYLE: {
         fontFamily: ['Helvetica', 'Arial'],
@@ -22,7 +24,7 @@ module.exports = {
     LINES_DISTANCE: 20,
     LINES_COLOR: 0xEEEEEE,
     // TODO: hide lines options (for now use 0 as width)
-    LINES_WIDTH: 10,
+    LINES_WIDTH: 0,
 
     BALL_COLOR: 0xEEEEEE,
     BALL_SIZE: 10,
